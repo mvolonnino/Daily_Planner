02889ec5de15f03d4bd46bb708b966ae70c3ecc5
Matt Volonnino <mvolonnino12@gmail.com>
2020-07-31 23:04:06 -0400

    dynamically added rows and columns to page to get layout of our daily planner

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
-  console.log("I am connected");
-
   // This shows todays date to a hardcode p tag below the Jumbotron
   var todayDate = moment().format("MMMM Do YYYY,");
   var pTodayDate = $("#todayDate").text("Today's Date: " + todayDate);
   pTodayDate.text(todayDate);
+  var textArray = [];
 
   // This updates the current time bellow the date
-  var todayTime = $("#currentDay");
+  var todayTime = $("#currentTime");
   var update = setInterval(function () {
     date = moment(new Date());
     // console.log("Today date is: ", date.format("h:mm:ss a"));
@@ -15,10 +14,58 @@ $(document).ready(function () {
   }, 1000);
   setInterval(update, 1000);
 
-  function create9rows() {
-    for (var i = 0; i <= 9; i++) {
-      var row = $('<div class=row">');
-      console.log("this is new div: ", divContainer);
+  // create the 9 rows for the table
+  for (var hour = 9; hour < 18; hour++) {
+    var divRow = $("<div>");
+    divRow.addClass("row mt-1 mb-2");
+    divRow.attr("value", [hour]);
+
+    // add the Time column
+    var colTime = $("<div>");
+    colTime.addClass("col-sm-2 border border-warning rounded pt-4");
+    colTime.attr("value", [hour]);
+    // to get correct 'am' or 'pm' on the time and not have it in military time
+    if (hour === 12) {
+      colTime.text([hour] + ":00 PM");
+    } else if (hour > 12) {
+      colTime.text([hour] - 12 + ":00 PM");
+    } else {
+      colTime.text([hour] + ":00 AM");
     }
+    divRow.append(colTime);
+    // add the Text column
+    var colText = $("<textarea>");
+    colText.addClass("col-sm-9 border border-secondary rounded pt-4");
+    colText.attr("id", "textArea");
+    colText.attr("value", [hour]);
+    divRow.append(colText);
+    // add the Save column
+    var colSave = $("<div>");
+    colSave.addClass("col-sm-1 btn btn-outline-info rounded pt-3");
+    colSave.attr("value", [hour]);
+    colSave.attr("type", "button");
+    colSave.text("Save");
+    divRow.append(colSave);
+
+    // add the rows and columns to the container div
+    $(".container").append(divRow);
+  }
+
+  // Click function for saveBtn
+  $(".btn").on("click", function () {
+    var btnClick = $(this).attr("value");
+    var textArea = $("#textArea").text();
+
+    console.log("This value was clicked: ", btnClick);
+
+    getText();
+  });
+
+  // grab the text info from our textarea
+  function getText() {
+    var textInfo = $("#textArea").val();
+    console.log("This is the text we input: ", textInfo);
+    textArray.push(textInfo);
+    console.log("This is our text info in an array: ", textArray);
   }
 });
